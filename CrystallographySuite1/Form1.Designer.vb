3f18ce7ef0e13b74a8cb03f5ec44499f7fa763da
juraj.ahel <juraj.ahel@NBW-IMP-29>
2016-07-22 02:31:40 +0200

    Add project files.

---
+++ CrystallographySuite1/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(108, 14)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(97, 21)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "96 (12 x 8)"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(108, 41)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(89, 21)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "48 (6 x 8)"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Checked = True
+        Me.RadioButton3.Location = New System.Drawing.Point(108, 68)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(89, 21)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "24 (6 x 4)"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Panel1.Location = New System.Drawing.Point(108, 111)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(705, 493)
+        Me.Panel1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(956, 662)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ CrystallographySuite1/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+
+    Private ButtonGrid(,) As System.Windows.Forms.Button
+    Private PlateType As String
+    Private PlateX, PlateY As Integer
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        'GetPlateType()
+
+        'FormGrid()
+
+    End Sub
+
+    Private Sub GetPlateType()
+
+        Select Case True
+            Case RadioButton1.Checked
+                PlateType = "96"
+                PlateX = 12
+                PlateY = 8
+            Case RadioButton2.Checked
+                PlateType = "48"
+                PlateX = 8
+                PlateY = 6
+            Case RadioButton3.Checked
+                PlateType = "24"
+                PlateX = 6
+                PlateY = 4
+        End Select
+
+    End Sub
+
+    'Private Sub PlateTypeSelection_Change(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged, RadioButton3.CheckedChanged
+    '    DestroyGrid()
+    '    GetPlateType()
+    '    FormGrid()
+    'End Sub
+
+    Private Sub DestroyGrid()
+
+        Dim i, j As Integer
+        Dim tempButton As System.Windows.Forms.Button
+
+        If ButtonGrid IsNot Nothing Then
+
+            For i = 0 To UBound(ButtonGrid, 1)
+                For j = 0 To UBound(ButtonGrid, 2)
+                    tempButton = ButtonGrid(i, j)
+                    If tempButton IsNot Nothing Then
+                        tempButton.Dispose()
+                    End If
+                Next
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetPlateType()
+        DestroyGrid()
+        FormGrid()
+    End Sub
+
+    Private Sub FormGrid()
+
+        Const Margin As Integer = 20
+        Const SpacingPercent As Double = 0.02
+
+        Dim i, j As Integer
+        Dim tempX, tempY As Integer
+        Dim ButtonSpacingX, ButtonSpacingY As Integer
+        Dim ButtonGapX, ButtonGapY As Integer
+        Dim tempButton As System.Windows.Forms.Button
+
+        Dim ButtonWidth, ButtonHeight As Integer
+
+        Dim Origin As Point
+
+        Origin = New Point(0, 0) 'Panel1.Location
+
+        ReDim ButtonGrid(PlateY - 1, PlateX - 1)
+
+        ButtonSpacingX = (Panel1.Size.Width - 2 * Margin) \ PlateX
+        ButtonSpacingY = (Panel1.Size.Height - 2 * Margin) \ PlateY
+
+        ButtonWidth = ButtonSpacingX * (1 - SpacingPercent)
+        ButtonHeight = ButtonSpacingY * (1 - SpacingPercent)
+
+        ButtonGapX = (ButtonSpacingX - ButtonWidth) \ 2
+        ButtonGapY = (ButtonSpacingY - ButtonHeight) \ 2
+
+
+        For i = 1 To PlateY
+            For j = 1 To PlateX
+                tempButton = New System.Windows.Forms.Button
+                tempX = Origin.X + Margin + ButtonGapX + (j - 1) * ButtonSpacingX
+                tempY = Origin.Y + Margin + ButtonGapY + (i - 1) * ButtonSpacingY
+
+                With tempButton
+                    .Location = New Point(tempX, tempY)
+                    .Size = New Size(ButtonWidth, ButtonHeight)
+                    .Text = Chr(64 + i) & j
+                    .BackColor = SystemColors.ControlLight
+                End With
+
+                ButtonGrid(i - 1, j - 1) = tempButton
+
+                Panel1.Controls.Add(tempButton)
+
+            Next
+        Next
+
+
+
+    End Sub
+
+
+End Class
